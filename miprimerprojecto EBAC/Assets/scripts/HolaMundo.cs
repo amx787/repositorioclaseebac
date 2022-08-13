17bf621efe024e25776f5fd5a6c98fcde0f4c48b
amx787 <isaac.colin@yahoo.com.mx>
2022-08-12 22:52:49 -0500

    Hola Mundo
    se agrego el script de hola mundo a la main camera
    -se utilizaron las funciones de debug para imprimir los diferentes tipos de mensajes

--- miprimerprojecto EBAC/Assets/scripts/HolaMundo.cs
+++ miprimerprojecto EBAC/Assets/scripts/HolaMundo.cs
@@ -7,7 +7,10 @@ public class HolaMundo : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        print("algo paso");
+        Debug.Log("Algo Paso");
+        Debug.LogWarning("Algo salio Medianamente Mal");
+        Debug.LogError("Algo Salio Muy Mal");
     }
 
     // Update is called once per frame
