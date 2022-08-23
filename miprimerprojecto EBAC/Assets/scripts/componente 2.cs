57c8c6654b24acd38813ecc28fdd701f3b4be59e
isaac colin soto <isaac.colin@yahoo.com.mx>
2022-08-23 14:42:39 -0500

    se efectuaron cambios
    se crearon 3 scripst

--- miprimerprojecto EBAC/Assets/scripts/componente 2.cs
+++ miprimerprojecto EBAC/Assets/scripts/componente 2.cs
@@ -2,26 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HolaMundo : MonoBehaviour
+public class componente2 : MonoBehaviour
 {
-    
-    int x;
-
     // Start is called before the first frame update
     void Start()
     {
-
-        x = 0;
-        print("algo paso");
         
-        Debug.LogWarning("Algo salio Medianamente Mal");
-        Debug.LogError("Algo Salio Muy Mal");
     }
 
     // Update is called once per frame
     void Update()
     {
-        x = x + 1;
-        Debug.Log(x);
+        
     }
 }
