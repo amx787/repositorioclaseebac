a9c09d3dc67dd62a337e426263d75a098b938f67
amx787 <isaac.colin@yahoo.com.mx>
2022-08-23 15:41:57 -0500

    SE ELIMNO  MIOBJETO
    mi objeto fue eliminado

--- miprimerprojecto EBAC/Assets/scripts/componente2.cs
+++ miprimerprojecto EBAC/Assets/scripts/componente2.cs
@@ -2,12 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class componente1 : MonoBehaviour
+public class componente2 : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private void Awake()
+    {
+          
+    }
+        // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log(componente1.miObjeto.name);
     }
 
     // Update is called once per frame
