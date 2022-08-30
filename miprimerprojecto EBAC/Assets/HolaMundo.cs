ed4f95400d4e428388dbd84b3d1097d185dcc6c3
amx787 <isaac.colin@yahoo.com.mx>
2022-08-30 18:38:52 -0500

    se agrego tarea
    tarea fue efectuada fixeudate lateupdate etc

--- miprimerprojecto EBAC/Assets/HolaMundo.cs
+++ miprimerprojecto EBAC/Assets/HolaMundo.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class HolaMundo : MonoBehaviour
 {
-    
+
     int x;
 
     // Start is called before the first frame update
@@ -13,7 +13,7 @@ public class HolaMundo : MonoBehaviour
 
         x = 0;
         print("algo paso");
-        
+
         Debug.LogWarning("Algo salio Medianamente Mal");
         Debug.LogError("Algo Salio Muy Mal");
     }
@@ -21,7 +21,25 @@ public class HolaMundo : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        x++;
-        Debug.Log(x);
+        //x++;
+        //Debug.Log(x);
+        Debug.Log("Hola desde Update");  
     }
+    private void FixedUpdate() 
+    {
+        Debug.LogWarning("hola desde Fixed Update cada 50 frames");      
+    } 
+    private void LateUpdate()
+    {
+        Debug.Log("hola desde Late Update");  
+    }
+    private void OnEnable()
+    {
+        Debug.LogWarning("el objeto ha sido habilitado");  
+    }
+    private void OnDisable()
+    {
+        Debug.LogWarning("el objeto ha sido inhabilitado");  
+    }
+
 }
