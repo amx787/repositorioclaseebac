8cadef7dab7ff2a01c1233676f47ef46360eeaa6
amx787 <isaac.colin@yahoo.com.mx>
2023-02-06 21:04:24 -0600

    se creo scrip modulo7
    se agregaron variables bool

--- miprimerprojecto EBAC/Assets/scripts/TareaM7Bool.cs
+++ miprimerprojecto EBAC/Assets/scripts/TareaM7Bool.cs
@@ -4,6 +4,20 @@ using UnityEngine;
 
 public class TareaM7Bool : MonoBehaviour
 {
+
+    bool variable1;
+    bool variable2;
+    int valor1 = 2;
+    int limiteInferior = 0;
+    int limiteSuperior = 1;
+    
+    enum SelecionColor
+    {
+        negro,
+        blanco
+    }
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -11,8 +25,21 @@ public class TareaM7Bool : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+     void FixedUpdate()
     {
-        
-    }
+        variable1 = true;
+        variable2 = false;
+        if (variable1 || variable2)
+        {
+            Debug.Log("La variable 1 es verdadero");  
+        }
+        valor1 = Random.Range(limiteInferior, limiteSuperior);
+        Debug.Log(valor1);
+        if (valor1 >= 0)
+        {
+            Debug.Log("el valor es positivo");
+                
+        }
+            
+    } 
 }
